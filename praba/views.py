44625fbd1522035988c126cc295788ccd624c28a
kalagarabhaskar <kalagarabhaskaraoking95@gmail.com>
2023-03-15 23:04:13 +0530

    first commit

---
+++ praba/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+def ntr(request):
+    return render(request,'ntr.html')
+
+
+
+def ram(request):
+    return render(request,'ram.html')
